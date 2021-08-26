432d9b437a3e2c3320a9bbd0c748716cf10b9c3c
sebastian24091998 <mollosebastian4@gmail.com>
2021-08-26 01:23:03 -0400

    practica3

---
+++ practica3/practica3.js
@@ -0,0 +1,24 @@
+/*(function()
+{
+    alert('Hola');
+
+}());*/
+ // ejercio 1
+function multiplicar()
+{
+    var numero1 = parseFloat(document.getElementById('num1').value);
+    var numero2 =parseFloat(document.getElementById('num2').value);
+    var numero3 =parseFloat(document.getElementById('num3').value);
+    
+    var resultado = numero1*numero2*numero3;
+
+    document.getElementById('res').value = resultado
+    //return resultado;
+}
+
+// Ejercicio 2
+
+
+
+
+
